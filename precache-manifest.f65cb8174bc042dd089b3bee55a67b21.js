19bbb3bfa58460c1317c5191728412161fa252ee
kavandalal003@gmail.com <kavandalal003@gmail.com>
2021-06-13 21:15:42 +0530

    Updates

---
+++ precache-manifest.f65cb8174bc042dd089b3bee55a67b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844cc841d83484574a271bf6f3a70ad4",
+    "url": "/react-advanced-2020/index.html"
+  },
+  {
+    "revision": "0be970bb8a94489c54d6",
+    "url": "/react-advanced-2020/static/css/main.e2744403.chunk.css"
+  },
+  {
+    "revision": "3ac27f6efea5dbf41542",
+    "url": "/react-advanced-2020/static/js/2.88bf9f60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-advanced-2020/static/js/2.88bf9f60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0be970bb8a94489c54d6",
+    "url": "/react-advanced-2020/static/js/main.7ba1f0e0.chunk.js"
+  },
+  {
+    "revision": "e3479f722fac79110411",
+    "url": "/react-advanced-2020/static/js/runtime-main.66de057a.js"
+  }
+]);
