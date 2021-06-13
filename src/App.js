d345de742d4c83bc0de1251e1480bcf0f714f34b
kavandalal003@gmail.com <kavandalal003@gmail.com>
2021-06-13 21:21:02 +0530

    2nd update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import Source from './tutorial/1-useState/setup/5-useState-counter'
+import Source11 from './tutorial/1-useState/setup/1-error-example'
+import Source12 from './tutorial/1-useState/setup/2-useState-basics'
+import Source13 from './tutorial/1-useState/setup/3-useState-array'
+import Source14 from './tutorial/1-useState/setup/4-useState-object'
+import Source15 from './tutorial/1-useState/setup/5-useState-counter'
 function App() {
-  return (
-    <Source/>
+  return (<>
+    <Source11/>
+    <Source12/>
+    <Source13/>
+    <Source14/>
+    <Source15/>
+  </>
   )
 }
 
