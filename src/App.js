3ed82bcd7080fc22ed1c1cfe14daa0b0c9d0d072
kavandalal003@gmail.com <kavandalal003@gmail.com>
2021-06-19 12:18:33 +0530

    until conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 //edit imports here 
 
-import Source23 from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
-// import Final21 from './tutorial/2-useEffect/final/2-useEffect-cleanup'
+import Source33 from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
+import Final33 from './tutorial/2-useEffect/final/3-useEffect-fetch-data'
 
 function App() {
   return (
   <>
   {/* Same as React.Fragment  */}
    
-    <Source23/>
-    {/* <Final21/> */}
+    <Source33/>
+    {/* <Final33/> */}
   </>
   )
 }
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,43 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
+
   // const firstValue = text || 'hello world';
+  // in || operator, if the 'text' is true then 'text' will be returned 
+  // in || operator, if the 'text' is false then 'hello world' will be returned 
+
+
   // const secondValue = text && 'hello world';
+  // in && operator, if the 'text' is true then 'hello world' will be returned 
+  // in && operator, if the 'text' is false then 'text' will be returned 
 
-  return <h2>short circuit</h2>;
+
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value : {secondValue}</h1> */}
+      
+      <h1>{text || 'john doe'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+
+      {/* simple ternary operator  */}
+      {isError ? 
+      // true part 
+      (
+        <p>there is an error...</p>
+      ) : 
+      // false part 
+      (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
+    </>
+  );
 };
-
 export default ShortCircuit;
